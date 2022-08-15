b787886b371bd23e920f7aa308fd309aa8638669
Angelo M <65518034+Norbory@users.noreply.github.com>
2022-08-15 12:57:52 -0500

    Cambie las cartas y empece el JS

---
+++ action.js
@@ -0,0 +1,10 @@
+function estudios() {
+    let titulo0 = document.querySelector(".info .carta2 h3");
+    let titulo1 = document.querySelector(".info .carta2 h5");
+    let imagen = document.querySelector(".info .carta2 img");
+    let fondo = document.querySelector(".info .carta2");
+    fondo.setAttribute("height","50%");
+    imagen.setAttribute("height","80%");
+    titulo0.setAttribute("visibility","visible");
+    titulo1.setAttribute("visibility","visible");
+}
